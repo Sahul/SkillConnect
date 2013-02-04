acd0ac1e27370b5c106777cc2ccb8ff272d76442
Sahul <sahul.peermohammed@gmail.com>
2013-02-04 13:34:42 -0600

    Add

---
+++ gen/edu/uta/SkillsConnect/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.uta.SkillsConnect;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/uta/SkillsConnect/SkillsConnectActivity.java
@@ -0,0 +1,13 @@
+package edu.uta.SkillsConnect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SkillsConnectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
